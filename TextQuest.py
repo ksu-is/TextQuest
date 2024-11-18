06cafb1e6e2aa0c7644140c0d214d915959e90b4
cmcmil22 <cmcmil22@students.kennesaw.edu>
2024-11-18 11:39:13 -0500

    Update TextQuest.py
    Made some edits to the wording, and also added more code to the project.

--- TextQuest.py
+++ TextQuest.py
@@ -1,5 +1,5 @@
 def start_game():
-    print("Welcome to TextQuest! You will be faced will multiple choices as time goes on. These choices will decide your fate.")
+    print("Welcome to TextQuest! You will be faced will multiple choices. These choices will decide your fate.")
     print("While you are walking a trail, you find yourself in front of a dark forest. What will you do?")
     
     while True:
@@ -66,3 +66,34 @@ def investigate_noise():
     print("You laugh out of nervousness, but decide it's time to keep going.")
     
     next_step()
+
+def deep_in_forest():
+    print("\nYou walk further into the forest. The air grows cold.")
+    print("Eventually, you come across a strange statue with unfamiliar wording.")
+    print("What will you do?")
+    
+    while True:
+        print("\n1. Examine the statue")
+        print("2. Touch the statue")
+        print("3. Turn back")
+        choice = input("What do you want to do? (1, 2, or 3): ").strip()
+
+        if choice == "1":
+            examine_statue()
+            break
+        elif choice == "2":
+            touch_statue()
+            break
+        elif choice == "3":
+            print("You decide to turn back. You have an even more strange feeling walking around.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+def climb_tree():
+    print("\nYou climb a nearby tree to get a better view of your surroundings.")
+    print("From up here, you can see a village far from the forest.")
+    print("You climb down the tree, ready to go find the village.")
+    
+    next_step()
+
